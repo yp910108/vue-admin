07049a489ff158474308686cebd723b8460a240f
yp910108 <1548018679@qq.com>
2019-01-02 08:42:57 +0800

    添加 normalize.css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'normalize.css/normalize.css'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Cookies from 'js-cookie'
