24879599e9831641a7c1ef0c74c678ebc418dd88
yp910108 <1548018679@qq.com>
2019-01-18 13:28:50 +0800

    401

--- src/router/index.js
+++ src/router/index.js
@@ -123,6 +123,36 @@ export const asyncRouterMap = [
     ]
   },
   {
+    path: '/error',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'ErrorPages',
+    meta: {
+      title: 'errorPages',
+      icon: '404'
+    },
+    children: [
+      {
+        path: '401',
+        component: () => import('@/views/error-page/401'),
+        name: 'Page401',
+        meta: {
+          title: 'page401',
+          noCache: true
+        }
+      },
+      {
+        path: '404',
+        component: () => import('@/views/error-page/404'),
+        name: 'Page404',
+        meta: {
+          title: 'page404',
+          noCache: true
+        }
+      }
+    ]
+  },
+  {
     path: '*',
     redirect: '/404',
     hidden: true
