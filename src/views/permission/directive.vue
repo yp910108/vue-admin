67eb29f4db24d359fbce47cd8b5faddef1d47d9e
yp910108 <1548018679@qq.com>
2019-01-18 10:34:19 +0800

    权限页面开发完成

--- src/views/permission/directive.vue
+++ src/views/permission/directive.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="app-container">
     <switch-roles @change="handleRolesChange"/>
-    <div style="margin-top: 30px;">
+    <div :key="key" style="margin-top: 30px;">
       <span v-permission="['admin']" class="permission-alert">
         Only
         <el-tag class="permission-tag" size="small">admin</el-tag> can see this
       </span>
       <span v-permission="['editor']" class="permission-alert">
         Only
-        <el-tag class="permision-tag" size="small">eidtor</el-tag> can see this
+        <el-tag class="permission-tag" size="small">editor</el-tag> can see this
       </span>
       <span v-permission="['admin', 'editor']" class="permission-alert">
         Both
@@ -16,9 +16,23 @@
         <el-tag class="permission-tag" size="small">editor</el-tag> can see this
       </span>
     </div>
+    <div :key="`checkPermission${key}`" style="margin-top: 30px;">
+      <code>
+        In some cases it is not suitable to use v-permission, such as element Tab component which can only be achieved by manually setting the v-if.
+        <br> e.g.
+      </code>
+      <el-tabs type="border-card" style="width: 500px;">
+        <el-tab-pane v-if="checkPermission(['admin'])" label="Admin">Admin can see this</el-tab-pane>
+        <el-tab-pane v-if="checkPermission(['editor'])" label="Editor">Editor can see this</el-tab-pane>
+        <el-tab-pane v-if="checkPermission(['admin','editor'])" label="Admin-OR-Editor">
+          Both admin or editor can see this
+        </el-tab-pane>
+      </el-tabs>
+    </div>
   </div>
 </template>
 <script>
+  import checkPermission from '@/utils/permission'
   import permission from '@/directive/permission'
   import SwitchRoles from './components/switch-roles'
 
@@ -30,12 +44,32 @@
     components: {
       SwitchRoles
     },
+    data() {
+      return {
+        key: 1 // 为了能每次切换权限的时候重新初始化指令
+      }
+    },
     methods: {
+      checkPermission,
       handleRolesChange() {
+        this.key++
       }
     }
   }
 </script>
 <style lang="scss" scoped>
-
+  .app-container {
+    .permission-alert {
+      display: block;
+      margin-top: 30px;
+      border-radius: 4px;
+      padding: 8px 16px;
+      width: 320px;
+      background-color: #f0f9eb;
+      color: #67c23a;
+    }
+    .permission-tag {
+      background-color: #ecf5ff;
+    }
+  }
 </style>
