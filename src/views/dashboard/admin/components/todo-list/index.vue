b973bda13c303e7a6acd1de7dc5fddc154c32b5e
yp910108 <1548018679@qq.com>
2019-01-21 23:00:59 +0800

    todolist

--- src/views/dashboard/admin/components/todo-list/index.vue
+++ src/views/dashboard/admin/components/todo-list/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>123</p>
+  <section class="todoapp">
+    <header class="header">
+      <input class="new-todo" autocomplete="off" placeholder="Todo List">
+      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <label for="toggle-all"/>
+    </header>
+    <ul class="todo-list"></ul>
+  </section>
 </template>
 
 <script>
@@ -7,5 +14,5 @@
 </script>
 
 <style lang="scss" scoped>
-
+  @import "./index";
 </style>
---
+++ src/views/dashboard/admin/components/todo-list/todo.vue
@@ -0,0 +1,22 @@
+<template>
+  <li :class="{completed: todo.done, editing}" class="todo">
+    <div class="view">
+      <input
+        :checked="todo.done"
+        class="toggle"
+        type="checkbox"
+      />
+      <label>{{todo.text}}</label>
+      <button class="destroy"></button>
+    </div>
+    <input/>
+  </li>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
