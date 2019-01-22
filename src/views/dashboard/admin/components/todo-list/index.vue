56b7d577a7d2c84d17e85fd04ca13f8c1911e153
yp910108 <1548018679@qq.com>
2019-01-22 23:44:43 +0800

    todo list

--- src/views/dashboard/admin/components/todo-list/index.vue
+++ src/views/dashboard/admin/components/todo-list/index.vue
@@ -1,16 +1,106 @@
 <template>
   <section class="todoapp">
     <header class="header">
-      <input class="new-todo" autocomplete="off" placeholder="Todo List">
-      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <input
+        class="new-todo"
+        autocomplete="off"
+        placeholder="Todo List"
+        @keyup.enter="addTodo"
+      >
+      <input
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        :checked="allChecked"
+        @change="toggleAll({done: !allChecked})"
+      >
       <label for="toggle-all"/>
     </header>
-    <ul class="todo-list"></ul>
+    <ul v-show="todos.length" class="todo-list">
+      <todo
+        v-for="(todo, index) in filteredTodos"
+        :key="index"
+        :todo="todo"
+        @toggle-todo="toggleTodo"
+        @edit-todo="editTodo"
+        @delete-todo="deleteTodo"
+      ></todo>
+    </ul>
   </section>
 </template>
 
 <script>
-  export default {}
+  import Todo from './todo'
+
+  const STORAGE_KEY = 'todos'
+  const defaultList = [
+    {text: 'star this repository', done: false},
+    {text: 'fork this repository', done: false},
+    {text: 'follow author', done: false},
+    {text: 'vue-element-admin', done: true},
+    {text: 'vue', done: true},
+    {text: 'element-ui', done: true},
+    {text: 'axios', done: true},
+    {text: 'webpack', done: true}
+  ]
+  const filters = {
+    all: (todos) => todos,
+    active: (todos) => todos.filter((todo) => !todo.done),
+    completed: (todos) => todos.filter((todo) => todo.done)
+  }
+  export default {
+    components: {
+      Todo
+    },
+    data() {
+      return {
+        visibility: 'all',
+        // todos: JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || defaultList
+        todos: defaultList
+      }
+    },
+    methods: {
+      setLocalStorage() {
+        window.localStorage.setItem(STORAGE_KEY, JSON.stringify(this.todos))
+      },
+      addTodo(e) {
+        const text = e.target.value.trim()
+        if (text) {
+          this.todos.unshift({
+            text,
+            done: false
+          })
+          this.setLocalStorage()
+        }
+        e.target.value = ''
+      },
+      toggleTodo(todo) {
+        todo.done = !todo.done
+      },
+      editTodo({todo, value}) {
+        todo.text = value
+        this.setLocalStorage()
+      },
+      deleteTodo(todo) {
+        this.todos.splice(this.todos.indexOf(todo), 1)
+        this.setLocalStorage()
+      },
+      toggleAll({done}) {
+        this.todos.forEach((todo) => {
+          todo.done = done
+        })
+        this.setLocalStorage()
+      }
+    },
+    computed: {
+      allChecked() {
+        return this.todos.every((todo) => todo.done)
+      },
+      filteredTodos() {
+        return filters[this.visibility](this.todos)
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
--- src/views/dashboard/admin/components/todo-list/todo.vue
+++ src/views/dashboard/admin/components/todo-list/todo.vue
@@ -1,22 +1,77 @@
 <template>
   <li :class="{completed: todo.done, editing}" class="todo">
-    <div class="view">
+    <div v-show="!editing" class="view">
       <input
         :checked="todo.done"
         class="toggle"
         type="checkbox"
+        @change="toggleTodo(todo)"
       />
-      <label>{{todo.text}}</label>
-      <button class="destroy"></button>
+      <label @dblclick="editing = true">{{todo.text}}</label>
+      <button class="destroy" @click="deleteTodo(todo)"></button>
     </div>
-    <input/>
+    <input
+      v-show="editing"
+      v-focus
+      :value="todo.text"
+      class="edit"
+      @keyup.enter="doneEdit"
+      @keyup.esc="cancelEdit"
+      @blur="doneEdit"
+    />
   </li>
 </template>
 
 <script>
-  export default {}
+  export default {
+    name: 'Todo',
+    props: {
+      todo: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+    },
+    directives: {
+      focus(el, binding, {context}) {
+        context.$nextTick(() => {
+          el.focus()
+        })
+      }
+    },
+    data() {
+      return {
+        editing: false
+      }
+    },
+    methods: {
+      toggleTodo(todo) {
+        this.$emit('toggle-todo', todo)
+      },
+      editTodo({todo, value}) {
+        this.$emit('edit-todo', {todo, value})
+      },
+      deleteTodo(todo) {
+        this.$emit('delete-todo', todo)
+      },
+      doneEdit(e) {
+        const value = e.target.value.trim()
+        const {todo} = this
+        if (!value) {
+          this.deleteTodo(todo)
+        } else {
+          this.editTodo({
+            todo,
+            value
+          })
+        }
+        this.editing = false
+      },
+      cancelEdit(e) {
+        e.target.value = this.todo.text
+        this.editing = false
+      }
+    }
+  }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
