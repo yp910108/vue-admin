50977fe384c88a8c627dc9f3610eded97b2a392e
yp910108 <1548018679@qq.com>
2019-01-23 21:45:27 +0800

    todo list 完成

--- src/views/dashboard/admin/components/todo-list/index.vue
+++ src/views/dashboard/admin/components/todo-list/index.vue
@@ -26,6 +26,17 @@
         @delete-todo="deleteTodo"
       ></todo>
     </ul>
+    <footer v-show="todos.length" class="footer">
+      <span class="todo-count">
+        <strong>{{remaining}}</strong>
+        {{remaining | pluralize('item')}} left
+      </span>
+      <ul class="filters">
+        <li v-for="(val, key) in filters" :key="key">
+          <a :class="{selected: visibility === key}" @click.prevent="visibility = key">{{key | capitalize}}</a>
+        </li>
+      </ul>
+    </footer>
   </section>
 </template>
 
@@ -56,7 +67,8 @@
       return {
         visibility: 'all',
         // todos: JSON.parse(window.localStorage.getItem(STORAGE_KEY)) || defaultList
-        todos: defaultList
+        todos: defaultList,
+        filters
       }
     },
     methods: {
@@ -92,12 +104,19 @@
         this.setLocalStorage()
       }
     },
+    filters: {
+      pluralize: (n, w) => n === 1 ? w : w + 's',
+      capitalize: (s) => s.charAt(0).toUpperCase() + s.slice(1)
+    },
     computed: {
       allChecked() {
         return this.todos.every((todo) => todo.done)
       },
       filteredTodos() {
         return filters[this.visibility](this.todos)
+      },
+      remaining() {
+        return this.todos.filter((todo) => !todo.done).length
       }
     }
   }
