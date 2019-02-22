1d2268a7b77e69b26ac23e763bb9865b331d300c
yp910108 <1548018679@qq.com>
2019-02-22 17:38:09 +0800

    drag-table

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,3 +1,5 @@
 export function toThousandFilter(num) {
   return (+num || 0).toString().replace(/^-?\d+/g, (m) => m.replace(/(?=(?!\b)(\d{3})+$)/g, ','))
 }
+
+export {parseTime, formatTime} from '@/utils'
